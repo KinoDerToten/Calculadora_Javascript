87f89a84478f6ebb0fd1d5efe0a0c93e5112d7ec
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-07-19 11:32:12 -0400

    criando a lógica dos botoes

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,20 @@
 onload = () => {
-    document.querySelecton('#bt-7').onclick = digito;
+    document.querySelector('#bt-0').onclick = () => digito(0);
+    document.querySelector('#bt-1').onclick = () => digito(1);
+    document.querySelector('#bt-2').onclick = () => digito(2);
+    document.querySelector('#bt-3').onclick = () => digito(3);
+    document.querySelector('#bt-4').onclick = () => digito(4);
+    document.querySelector('#bt-5').onclick = () => digito(5);
+    document.querySelector('#bt-6').onclick = () => digito(6);
+    document.querySelector('#bt-7').onclick = () => digito(7);
+    document.querySelector('#bt-8').onclick = () => digito(8);
+    document.querySelector('#bt-8').onclick = () => digito(9);
+
 }
 
-const digito = () => {
-    
+// Variaveis para armazenar o valor do operador e o estado da calculadora
+let sValor = '';
+
+const digito = (n) => {
+    console.log(n);
 }
