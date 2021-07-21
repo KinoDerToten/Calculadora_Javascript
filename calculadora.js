7cf74b173ace04d3ddf318e788ecdd75134fdc8b
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-07-21 09:24:36 -0400

    Criando o tratamento do botao de ponto decimal e all clear

--- calculadora.js
+++ calculadora.js
@@ -9,21 +9,41 @@ onload = () => {
     document.querySelector('#bt-7').onclick = () => digito(7);
     document.querySelector('#bt-8').onclick = () => digito(8);
     document.querySelector('#bt-8').onclick = () => digito(9);
-
+    document.querySelector('#bt-comma').onclick = virgula;
+    document.querySelector('#bt-ac').onclick = limpa;
 }
 
 // Variaveis para armazenar o valor do operador e o estado da calculadora
 let sValor = '0';
 let ehNovoNumero = true;
+let ehNumeroFalse = false;
 
 const atualizaVisor = () => {
     document.querySelector('#display').innerText = sValor;
 }
 
+// Tratamento do clique no botao de digito
 const digito = (n) => {
     if (ehNovoNumero) {
         sValor = '' + n;
         ehNovoNumero = false;
-    } else sValor += n;
+    } else {sValor += n;}
+    atualizaVisor();
+}
+
+// Tratamento do clique no botão de ponto decimal
+const virgula = () =>  {
+    if (ehNovoNumero) {
+        sValor = '0,'
+    } else if (sValor.indexOf(',') == -1){
+        sValor += ','
+        }
+    atualizaVisor();
+}
+
+// Tratamento do clique do botao AC (All Clear)
+const limpa = () => {
+    ehNovoNumero = true;
+    sValor = '0';
     atualizaVisor();
 }
